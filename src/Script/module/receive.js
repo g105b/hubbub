f0cb4eda563c4180a0bdbbc5b640497d3f89ab64
Greg Bowler <greg.bowler@g105b.com>
2016-03-20 09:26:50 +0000

    Play sound file logic

--- src/Script/module/receive.js
+++ src/Script/module/receive.js
@@ -14,7 +14,7 @@ function poll() {
 		xhr = new XMLHttpRequest(),
 	$$;
 
-	xhr.open("GET", "/receive?time=" + (+dateLastListened));
+	xhr.open("GET", "/receive?time=" + ~~(+dateLastListened / 1000));
 	xhr.addEventListener("load", poll_load);
 	xhr.send();
 }
@@ -22,7 +22,19 @@ function poll() {
 function poll_load() {
 	var
 		obj = JSON.parse(this.responseText),
+		i,
 	$$;
+
+	for(i = 0, len = obj.length; i < len; i++) {
+		play(obj[i].path);
+	}
+
+	dateLastListened = new Date();
+	setTimeout(poll, 2000);
+}
+
+function play(path) {
+	console.log("PLAYING: " + path);
 }
 
 return {
