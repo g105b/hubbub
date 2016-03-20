1c27c9dbc47e3400c2dd5eef763911a2da906bb8
Greg Bowler <greg.bowler@g105b.com>
2016-03-20 08:41:45 +0000

    extract transmit code

--- src/Script/hubbub.js
+++ src/Script/hubbub.js
@@ -8,72 +8,25 @@ var
 $$;
 
 ;(function go() {
-	talkBtn.addEventListener("mousedown", talk_start);
-	talkBtn.addEventListener("mouseup", talk_end);
+	hubbub.transmit.init();
 
-	try {
-		// webkit shim
-		window.AudioContext =
-			window.AudioContext || window.webkitAudioContext;
-		navigator.getUserMedia =
-			navigator.getUserMedia || navigator.webkitGetUserMedia;
-		window.URL = window.URL || window.webkitURL;
+	talkBtn.addEventListener("mousedown", mousedown);
+	talkBtn.addEventListener("mouseup", mouseup);
 
-		audio_context = new AudioContext;
-
-		console.log('Audio context set up.');
-		console.log('navigator.getUserMedia '
-			+ (navigator.getUserMedia ? 'available.' : 'not present!'));
-	}
-	catch(e) {
-		alert('No web audio support in this browser!');
-	}
-
-	navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
-		console.log('No live audio input: ' + e);
-	});
+	hubbub.transmit.setCallback(complete);
 })();
 
-function startUserMedia(stream) {
-	var input = audio_context.createMediaStreamSource(stream);
-	console.log('Media stream created.');
-
-	// Uncomment if you want the audio to feedback directly
-	//input.connect(audio_context.destination);
-
-	recorder = new Recorder(input);
-
-	console.log('Recorder initialised.');
+function mousedown() {
+	hubbub.transmit.talk_start();
 }
 
-function talk_start() {
-	recorder && recorder.record();
-	console.log("recording...");
-}
-
-function talk_end() {
-	recorder && recorder.stop();
-	upload();
+function mouseup() {
+	console.log("mouse is up!");
+	hubbub.transmit.talk_end();
 	talkBtn.disabled = true;
 }
 
-function upload() {
-	recorder && recorder.exportWAV(function(blob) {
-		console.log(blob);
-
-		var
-			fd = new FormData(),
-			xhr = new XMLHttpRequest(),
-		$$;
-
-		fd.append("sound", blob);
-		xhr.open("POST", "/transmit");
-		xhr.addEventListener("load", upload_cb);
-		xhr.send(fd);
-	});
-}
-
-function upload_cb() {
+function complete() {
 	talkBtn.disabled = false;
 }
 
